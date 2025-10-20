5d348121b03d178fbea1ce1d68f939c46134b302
Miraç Ö. AĞCABAY <miracagcabayy@gmail.com>
2025-10-21 00:28:13 +0300

    Add files via upload

--- Task-1-HosgeldinMesajlari/UcMesaj.java
+++ Task-1-HosgeldinMesajlari/UcMesaj.java
@@ -3,7 +3,7 @@ package Ekim17Hafta3.Gorev1;
 public class UcMesaj {
     public static void main(String[] args) {
         System.out.println("Java'ya Hoş Geldiniz ");
-        System.out.println("Bilgisayar Bilimine Hoş Grldiniz ");
+        System.out.println("Bilgisayar Bilimine Hoş Geldiniz ");
         System.out.println("Programlama Eğlencelidir. ");
     }
 }
