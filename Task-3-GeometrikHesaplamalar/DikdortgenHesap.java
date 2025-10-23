d7b5b3a85ad553708ecaffafe49bdd60bc4565b1
Miraç Ö. AĞCABAY <miracagcabayy@gmail.com>
2025-10-23 13:22:49 +0300

    Update DikdortgenHesap.java

---
+++ Task-3-GeometrikHesaplamalar/DikdortgenHesap.java
@@ -0,0 +1,14 @@
+package Ekim17Hafta3.Gorev3;
+
+public class DikdörgenHesap {
+    public static void main(String[] args) {
+        double en = 4.5;
+        double boy= 7.9;
+
+        double alan=en*boy;
+        double cevre= 2*(en+boy);
+
+        System.out.println("Dikdörtgenin Alanı: "+ alan);
+        System.out.println("Diktörgenin Çevresi: "+ cevre);
+    }
+}
