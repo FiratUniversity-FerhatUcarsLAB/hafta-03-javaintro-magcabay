651ec8dafb2f21752c2bed94800302fb723d705b
Miraç Ö. AĞCABAY <miracagcabayy@gmail.com>
2025-10-23 13:29:50 +0300

    Update AsciiArtHarf.java

--- Task-4-StringDesenler/AsciiArtHarf.java
+++ Task-4-StringDesenler/AsciiArtHarf.java
@@ -2,12 +2,16 @@ package Ekim17Hafta3.Gorev4;
 
 public class AaciiArtHarf {
     public static void main(String[] args) {
-        System.out.println("M       M");
-        System.out.println("MM     MM");
-        System.out.println("M M   M M");
-        System.out.println("M  M M  M");
-        System.out.println("M   M   M");
-        System.out.println("M       M");
-        System.out.println("M       M");
+        String harfM = "M       M\n" +
+                "MM     MM\n" +
+                "M M   M M\n" +
+                "M  M M  M\n" +
+                "M   M   M\n" +
+                "M       M\n" +
+                "M       M";
+
+
+        System.out.println(harfM);
+
     }
 }
