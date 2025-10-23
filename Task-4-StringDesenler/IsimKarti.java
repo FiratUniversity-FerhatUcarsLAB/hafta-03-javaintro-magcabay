3823bd3661f5911914decb115ec986153a4c8006
Miraç Ö. AĞCABAY <miracagcabayy@gmail.com>
2025-10-23 13:30:40 +0300

    Update IsimKarti.java

---
+++ Task-4-StringDesenler/IsimKarti.java
@@ -0,0 +1,19 @@
+package Ekim17Hafta3.Gorev4;
+
+public class İsimKarti {
+    public static void main(String[] args) {
+        String ad = "Mirac ozcan ";
+        String soyad= "Agcabay";
+        String ogrNo = "250541002";
+        String bolum= "Yazilim Mühendisligi";
+
+        System.out.println("===============================");
+        System.out.println("    OGRENCİ BİLGİLERİ  ");
+        System.out.println("===============================");
+        System.out.println("Ad     : " + ad);
+        System.out.println("Soyad  : " + soyad);
+        System.out.println("No     : " + ogrNo);
+        System.out.println("Bolum  : " + bolum);
+        System.out.println("===============================");
+    }
+}
