fc268b32f237c92c047663ee455a2a37cf4afd2b
Miraç Ö. AĞCABAY <miracagcabayy@gmail.com>
2025-10-23 13:22:28 +0300

    Update DaireHesap.java

---
+++ Task-3-GeometrikHesaplamalar/DaireHesap.java
@@ -0,0 +1,15 @@
+package Ekim17Hafta3.Gorev3;
+
+public class DaireHesap {
+    public static void main(String[] args) {
+        double yaricap=5.5;
+        double pi=3.14159;
+
+        double alan= pi*yaricap*yaricap;
+        double cevre= 2*pi*yaricap;
+
+        System.out.println("Dairenin Alanı: "+ alan);
+        System.out.println("Dairenin Çevresi: "+ cevre);
+
+    }
+}
