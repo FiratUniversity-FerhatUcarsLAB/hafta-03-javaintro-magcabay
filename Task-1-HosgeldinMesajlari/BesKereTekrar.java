85ee80152addf766343b6aca4a202296df4ed44d
Miraç Ö. AĞCABAY <miracagcabayy@gmail.com>
2025-10-23 13:12:09 +0300

    Update BesKereTekrar.java

---
+++ Task-1-HosgeldinMesajlari/BesKereTekrar.java
@@ -0,0 +1,12 @@
+package Ekim17Hafta3.Gorev1;
+
+public class BesKereTekrar {
+    public static void main(String[] args) {
+        System.out.println("Java'ya Hoş Geldiniz. ");
+        System.out.println("Java'ya Hoş Geldiniz. ");
+        System.out.println("Java'ya Hoş Geldiniz. ");
+        System.out.println("Java'ya Hoş Geldiniz. ");
+        System.out.println("Java'ya Hoş Geldiniz. ");
+
+    }
+}
