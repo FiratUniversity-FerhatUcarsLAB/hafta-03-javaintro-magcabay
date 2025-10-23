b137755fa5916aa5186bdc0545b0159a0e9707c7
Miraç Ö. AĞCABAY <miracagcabayy@gmail.com>
2025-10-23 13:15:44 +0300

    Update Faktoriyel.java

---
+++ Task-2-Matematikselislemler/Faktoriyel.java
@@ -0,0 +1,8 @@
+package Ekim17Hafta3.Gorev2;
+
+public class Faktöriyel {
+    public static void main(String[] args) {
+        int faktöriyel = 5*4*3*2*1;
+        System.out.println("Faktöriyelin Sonucu: "+ faktöriyel);
+    }
+}
