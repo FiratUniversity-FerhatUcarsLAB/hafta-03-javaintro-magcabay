e1d775a64f32d892d382753dfc94c93041b0fa9a
Miraç Ö. AĞCABAY <miracagcabayy@gmail.com>
2025-10-23 13:50:44 +0300

    Update CarpimTablosu.java

---
+++ Task-5-TabloDesenleri/CarpimTablosu.java
@@ -0,0 +1,22 @@
+package Ekim17Hafta3.Gorev5;
+
+public class CarpimTablosu {
+    public static void main(String[] args) {
+        System.out.println("2’nin Çarpım Tablosu");
+        System.out.println("n   2 * n");
+        System.out.println("-----------");
+
+        int n1=1, n2=2, n3=3, n4=4, n5=5, n6=6, n7=7, n8=8, n9=9, n10=10;
+
+        System.out.println(n1 + "    " + (2*n1));
+        System.out.println(n2 + "    " + (2*n2));
+        System.out.println(n3 + "    " + (2*n3));
+        System.out.println(n4 + "    " + (2*n4));
+        System.out.println(n5 + "    " + (2*n5));
+        System.out.println(n6 + "    " + (2*n6));
+        System.out.println(n7 + "    " + (2*n7));
+        System.out.println(n8 + "    " + (2*n8));
+        System.out.println(n9 + "    " + (2*n9));
+        System.out.println(n10 + "    " + (2*n10));
+    }
+}
