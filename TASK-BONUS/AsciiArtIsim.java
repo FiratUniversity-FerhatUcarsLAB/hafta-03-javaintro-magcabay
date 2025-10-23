4e8564ed5ccefcc8bfd1612625db02953562c8cd
Miraç Ö. AĞCABAY <miracagcabayy@gmail.com>
2025-10-23 14:04:19 +0300

    Update AsciiArtIsim.java

---
+++ TASK-BONUS/AsciiArtIsim.java
@@ -0,0 +1,14 @@
+package Ekim17Hafta3.BonusGorevler;
+
+public class AsciiArtIsim {
+    public static void main(String[] args) {
+        System.out.println("*     *    ******");
+        System.out.println("**   **    *    *");
+        System.out.println("* * * *    ******");
+        System.out.println("*  *  *    *    *");
+        System.out.println("*     *    *    *");
+        System.out.println();
+        System.out.println("      M.A");
+    }
+}
+
