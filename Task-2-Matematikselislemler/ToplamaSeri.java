4d5e6a901758c8e9be4fd0692d4515175bf8fd1b
Miraç Ö. AĞCABAY <miracagcabayy@gmail.com>
2025-10-23 13:16:51 +0300

    Update ToplamaSeri.java

---
+++ Task-2-Matematikselislemler/ToplamaSeri.java
@@ -0,0 +1,8 @@
+package Ekim17Hafta3.Gorev2;
+
+public class ToplamaSerisi {
+    public static void main(String[] args) {
+        int sonuc= 1+2+3+4+5+6+7+8+9;
+        System.out.println("1+2+3+...+9= " +sonuc);
+    }
+}
